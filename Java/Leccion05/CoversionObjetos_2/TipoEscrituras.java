4aa2e94fcf62b09bc18b8e46f8b1e835ff347953
Pluma22 <111616617+Pluma22@users.noreply.github.com>
2023-05-23 18:52:54 -0300

    Leccion 5 Java - Conversion de Objetos 2

---
+++ Java/Leccion05/CoversionObjetos_2/TipoEscrituras.java
@@ -0,0 +1,17 @@
+
+package domain;
+
+public enum TipoEscrituras {
+    CLASICO ("Escritura a mano"),
+    MODERNO ("Escritura digital");
+    
+    private final String descripcion;
+    // CONSTRUCTOR
+    private TipoEscrituras(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    // METODO GET
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+}
