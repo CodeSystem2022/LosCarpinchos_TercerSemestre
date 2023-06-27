bc19db013b2bd41db19ae13f6be85151736e6a7e
Sanp74 <92418215+Sanp74@users.noreply.github.com>
2023-06-27 16:49:40 -0300

    Update cursor_del_pool.py

--- python/Clase 11/cursor_del_pool.py
+++ python/Clase 11/cursor_del_pool.py
@@ -25,6 +25,12 @@ class CursorDelPool:
         self._cursor.close()
         Conexion.liberarConexion(self._conexion)
 
+if __name__=='__main__':
+    with CursorDelPool()as cursor:
+        log.debug('Dentro del bloque with')
+        cursor.execute('SELECT * FROM persona')
+        log.debug(cursor.fetchall())
+
 
 
 
